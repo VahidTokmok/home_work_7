f7766db2c0378af2fa15ae7ab28ba30f323708f0
VahidTokmok <mirvakhid100197@yandex.ru>
2023-01-22 23:30:54 +0600

    read

--- src/Hero.java
+++ src/Hero.java
@@ -3,6 +3,30 @@ public abstract class Hero implements HavingSuperAbility{
     public int damage;
     public String superAbility;
 
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getSuperAbility() {
+        return superAbility;
+    }
+
+    public void setSuperAbility(String superAbility) {
+        this.superAbility = superAbility;
+    }
+
     public Hero(int health, int damage, String superAbility) {
         this.health = health;
         this.damage = damage;
